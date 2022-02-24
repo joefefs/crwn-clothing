a90e60a3768d39e0f29b565973f257c234f61049
josefefs <design.joseferrer@gmail.com>
2022-02-23 21:42:45 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import HomePage from './pages/homepage/homepage.component';
+
+function App() {
+  return (
+    <div>
+     <HomePage />
+
+      
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './homepage.style.scss'
+import Diretory from '../../components/directory/directory.component'
+const HomePage = () => {
+    return (
+    <div className='homepage'>
+        <Diretory />  
+    </div>
+    )
+}
+
+export default HomePage
