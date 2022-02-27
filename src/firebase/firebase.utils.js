f86751b79a51b53747a6c5e613f406d46aa93d9a
josefefs <design.joseferrer@gmail.com>
2022-02-27 02:05:23 -0600

    added email sign up, added sign-up component, added firebase and firestore auth

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,6 +11,29 @@ const config = {
     appId: "1:623129056512:web:c41959c1c869b2aa02cfe5"
   };
 
+  export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if(!userAuth) return; //if the object of userAuth (returned by firestore), does not exist, return, cut the function.
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`)
+    const snapShot = await userRef.get()
+    console.log(snapShot)
+    if(!snapShot.exists) {
+      const { displayName, email } = userAuth
+      const createdAt = new Date()
+      try {
+        await userRef.set({
+          displayName,
+          email,
+          createdAt,
+          ...additionalData
+        })
+      } catch (error) {
+        console.log('error creating user ', error.message)
+      }
+    }
+    return userRef
+  }
+
   firebase.initializeApp(config)
 
   export const auth = firebase.auth()
