ee1094f8b768a829eca637ebcec0d59675372c1a
josefefs <design.joseferrer@gmail.com>
2022-02-27 20:20:36 -0600

    added redux logger and redux library and startet writing root reducer, user reducer and user action

---
+++ src/redux/user/user.reducer.js
@@ -0,0 +1,18 @@
+const INITIAL_STATE = {
+    currentUser: null
+}
+const userReducer = ( state = INITIAL_STATE, action) => { //if there's no value for state, it will set to currentUser: null
+    switch(action.type){ //
+        case 'SET_CURRENT_USER' :  //if the case is this one, then do this.... 
+        //It has to match by type (if that's the case, return the object with currentUser modified)
+            return {
+                ...state,
+                currentUser: action.payload
+            }
+        default : //if it isn't the case above, return state as is...
+          return state
+
+    }
+}
+
+export default userReducer;
