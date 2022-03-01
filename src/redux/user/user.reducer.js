dfa242737cd947ce3045854a12f6e716fa4edb5a
josefefs <design.joseferrer@gmail.com>
2022-03-01 01:06:12 -0600

    blocked rendering of sign in when signed in (using render props and Redirect from react router. Created userActioTypes to have consistency among files

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,9 +1,10 @@
+import { UserActionTypes } from "./user.types"
 const INITIAL_STATE = {
     currentUser: null
 }
 const userReducer = ( state = INITIAL_STATE, action) => { //if there's no value for state, it will set to currentUser: null
     switch(action.type){ //
-        case 'SET_CURRENT_USER' :  //if the case is this one, then do this.... 
+        case UserActionTypes.SET_CURRENT_USER :  //if the case is this one, then do this.... 
         //It has to match by type (if that's the case, return the object with currentUser modified)
             return {
                 ...state,
